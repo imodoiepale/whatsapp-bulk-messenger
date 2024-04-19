10deae64ea2c4c4a6d8e11d82abefefae19e096d
imodoiepale <ijepale@gmail.com>
2024-04-19 16:02:35 +0300

    yes

--- automator.py
+++ automator.py
@@ -1,90 +1,53 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
-from urllib.parse import quote
-import os
+from playwright.sync_api import sync_playwright
 
-options = Options()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-options.add_argument("--profile-directory=Default")
-options.add_argument("--user-data-dir=/var/tmp/chrome_user_data")
+def open_whatsapp_web():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://web.whatsapp.com/")
+        
+        page.get_by_role("button", name="Link with phone number", exact=True).click()
+        page.get_by_label("Type your phone number.").click()
+        page.get_by_label("Type your phone number.").fill("+254 743 854888")
+        page.get_by_role("button", name="Next").click()
 
-os.system("")
-os.environ["WDM_LOG_LEVEL"] = "0"
-class style():
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    UNDERLINE = '\033[4m'
-    RESET = '\033[0m'
+        # Wait for the div element to appear (optional)
+        page.wait_for_selector("div[aria-details='link-device-phone-number-code-screen-instructions']")
 
-print(style.BLUE)
-print("**********************************************************")
-print("**********************************************************")
-print("*****                                               ******")
-print("*****  THANK YOU FOR USING WHATSAPP BULK MESSENGER  ******")
-print("*****      This tool was built by Anirudh Bagri     ******")
-print("*****           www.github.com/anirudhbagri         ******")
-print("*****                                               ******")
-print("**********************************************************")
-print("**********************************************************")
-print(style.RESET)
+        # Get the div element
+        data_link_code_element = page.query_selector("div[aria-details='link-device-phone-number-code-screen-instructions']")
 
-f = open("message.txt", "r", encoding="utf8")
-message = f.read()
-f.close()
+        if data_link_code_element:
+            # Get the data-link-code attribute value
+            data_link_code_value = data_link_code_element.get_attribute("data-link-code")
 
-print(style.YELLOW + '\nThis is your message-')
-print(style.GREEN + message)
-print("\n" + style.RESET)
-message = quote(message)
+            if data_link_code_value:
+                print(f"get_data_link_code: {data_link_code_value}")
+            else:
+                print("data-link-code attribute not found or empty.")
+        else:
+            print("Div element with aria-details='link-device-phone-number-code-screen-instructions' not found.")
+        
+        # Prompt for user confirmation
+        confirmation = input("Have you input the code on your phone? (yes/no): ")
+        if confirmation.lower() == "yes":
+            page.get_by_role("textbox", name="Search input textbox").fill("⊙ (You)")
+            
+            # Find the element with translateY(72px) style attribute value
+            element = page.query_selector('div[style="transform: translateY(72px);"]')
 
-numbers = []
-f = open("numbers.txt", "r")
-for line in f.read().splitlines():
-	if line.strip() != "":
-		numbers.append(line.strip())
-f.close()
-total_number=len(numbers)
-print(style.RED + 'We found ' + str(total_number) + ' numbers in the file' + style.RESET)
-delay = 30
+            if element:
+                # Click on the element
+                element.click()
+            else:
+                print("Element not found")
+                
+            page.get_by_role("textbox", name="Type a message").fill("Hi James, Master Developer")
+            page.get_by_label("Send").click()
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-print('Once your browser opens up sign in to web whatsapp')
-driver.get('https://web.whatsapp.com')
-input(style.MAGENTA + "AFTER logging into Whatsapp Web is complete and your chats are visible, press ENTER..." + style.RESET)
-for idx, number in enumerate(numbers):
-	number = number.strip()
-	if number == "":
-		continue
-	print(style.YELLOW + '{}/{} => Sending message to {}.'.format((idx+1), total_number, number) + style.RESET)
-	try:
-		url = 'https://web.whatsapp.com/send?phone=' + number + '&text=' + message
-		sent = False
-		for i in range(3):
-			if not sent:
-				driver.get(url)
-				try:
-					click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='compose-btn-send']")))
-				except Exception as e:
-					print(style.RED + f"\nFailed to send message to: {number}, retry ({i+1}/3)")
-					print("Make sure your phone and computer is connected to the internet.")
-					print("If there is an alert, please dismiss it." + style.RESET)
-				else:
-					sleep(1)
-					click_btn.click()
-					sent=True
-					sleep(3)
-					print(style.GREEN + 'Message sent to: ' + number + style.RESET)
-	except Exception as e:
-		print(style.RED + 'Failed to send message to ' + number + str(e) + style.RESET)
-driver.close()
+        page.wait_for_timeout(0)
+        page.close()
+
+if __name__ == "__main__":
+    open_whatsapp_web()
